d5cfdd495450f0808bcb5ace1c944dc73f793172
Vika <kaledaviktoriya@gmail.com>
2018-10-10 22:32:39 +0300

    Day 01: Add one test method for each sorting algorithm

---
+++ NET.W.2018.Kaleda.01/SortingMSTests/SortingTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Sorting.Sorting;
+
+namespace SortingMSTests
+{
+	[TestClass]
+	public class SortingTests
+	{
+		[TestMethod]
+		public void QuickSortTest_ValidData_ValidResult()
+		{
+			// Generate array of random integers
+			Random r = new Random();
+			int[] array = Enumerable.Range(0, 100)
+				.Select(_ => r.Next(0, 100))
+				.ToArray(),
+				expected = new int[array.Length];
+
+			// Get two equals array's objects
+			Array.Copy(array, expected, array.Length);
+
+			// Sort the first with build-in method
+			Array.Sort(expected);
+
+			// Sort the second with testing method
+			QuickSort(array);
+
+			CollectionAssert.AreEqual(array, expected);
+		}
+
+		[TestMethod]
+		public void MergeSortTest_ValidData_ValidResult()
+		{
+			// Generate array of random integers
+			Random r = new Random();
+			int[] array = Enumerable.Range(0, 100)
+				.Select(_ => r.Next(0, 100))
+				.ToArray(),
+				expected = new int[array.Length];
+
+			// Get two equals array's objects
+			Array.Copy(array, expected, array.Length);
+
+			// Sort the first with build-in method
+			Array.Sort(expected);
+
+			// Sort the second with testing method
+			MergeSort(array);
+
+			CollectionAssert.AreEqual(array, expected);
+		}
+	}
+}
