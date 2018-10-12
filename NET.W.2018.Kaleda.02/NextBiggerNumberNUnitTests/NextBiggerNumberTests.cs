c0719ac00aa637b56683439b9eb18f5990a3a587
Vika <kaledaviktoriya@gmail.com>
2018-10-12 19:56:21 +0300

    Day 02: Add NUnit tests for NextBiggerNumber class

---
+++ NET.W.2018.Kaleda.02/NextBiggerNumberNUnitTests/NextBiggerNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NextBiggerNumberNUnitTests
+{
+	[TestFixture]
+	public class NextBiggerNumberTests
+	{
+		[Test]
+		[TestCase(12, 21)]
+		[TestCase(513, 531)]
+		[TestCase(2017, 2071)]
+		[TestCase(414, 441)]
+		[TestCase(144, 414)]
+		[TestCase(1234321, 1241233)]
+		[TestCase(1234126, 1234162)]
+		[TestCase(3456432, 3462345)]
+		[TestCase(10, -1)]
+		[TestCase(20, -1)]
+		public void FindNextBiggerNumber(int number, int expectedResult)
+		{
+			int result = NextBiggerNumber.NextBiggerNumber.FindNextBiggerNumber(number);
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+	}
+}
