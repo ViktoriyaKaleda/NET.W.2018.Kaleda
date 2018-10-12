f717c982a0fc2b0aac8fbd9aed7ad798506d6972
Vika <kaledaviktoriya@gmail.com>
2018-10-12 18:08:35 +0300

    Day 02: Add MSTests for BitsReplacement class

---
+++ NET.W.2018.Kaleda.02/BitsReplacementMSTests/BitsReplacementTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitsReplacementMSTests
+{
+	[TestClass]
+	public class BitsReplacementTests
+	{
+		[TestMethod]
+		[DataRow(15, 15, 0, 0, 15)]
+		[DataRow(8, 15, 0, 0, 9)]
+		[DataRow(8, 15, 3, 8, 120)]
+		[DataRow(123, 75, 2, 5, 111)]
+		[DataRow(-123, 10, 5, 8, -187)]
+		public void InsertBits_ValidCases_NumberWithReplacedBits(int firstNumber, int secondNumber, int i, int j, int expectedResult)
+		{
+			int result = BitsReplacement.BitsReplacement.InsertBits(firstNumber, secondNumber, i, j);
+
+			Assert.AreEqual(result, expectedResult);
+		}
+	}
+}
