3115c0861a5cba1ab99f39d515b2ace37c96405b
Vika <kaledaviktoriya@gmail.com>
2018-10-13 13:42:32 +0300

    Day 02: Add NUnit tests for FilterDigit when numbers are negative

--- NET.W.2018.Kaleda.02/FilterNUnitTests/FilterTests.cs
+++ NET.W.2018.Kaleda.02/FilterNUnitTests/FilterTests.cs
@@ -9,7 +9,8 @@ namespace FilterNUnitTests
 	{
 		[Test]
 		[TestCase(7, new int[] { 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17}, new int[] {7, 70, 17})]
-		[TestCase(0, new int[] { 0, 2, 201, 200, 21, 70, 15, 17 }, new int[] { 0, 201, 200, 70 })]
+		[TestCase(0, new int[] { 0, 2, 201, 200, 21, -70, -15, 17 }, new int[] { 0, 201, 200, -70 })]
+		[TestCase(2, new int[] { 0, 2, -201, 200, -21, 70, -15, 17 }, new int[] { 2, -201, 200, -21})]
 		[TestCase(1, new int[] { 0 }, new int[] { })]
 		[TestCase(1, new int[] {  }, new int[] { })]
 		public void FilterDigit_ValidDigitAndArrayOfNumbersToSort_FilteredList(int digit, int[] list, IEnumerable<int> expectedResult)
