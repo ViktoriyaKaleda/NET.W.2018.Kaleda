5f48a711c3ca735a8c6d9341701160c175cea0d8
Vika <kaledaviktoriya@gmail.com>
2018-10-13 13:32:46 +0300

    Day 02: Add xml docs for Filter class

--- NET.W.2018.Kaleda.02/Filter/Filter.cs
+++ NET.W.2018.Kaleda.02/Filter/Filter.cs
@@ -8,12 +8,18 @@ namespace Filter
 {
     public static class Filter
     {
-		public static int[] FilterDigit(int digit, params int[] list)
+		/// <summary>
+		/// Filters the array of numbers and returns array that only contains the specified digit.
+		/// </summary>
+		/// <param name="digit">Digit which every element in returns array should contains.</param>
+		/// <param name="array">Array to filter.</param>
+		/// <returns>The filtered array every element of which contains specified digit.</returns>
+		public static int[] FilterDigit(int digit, params int[] array)
 		{
-			CheckParametrs(digit, list);
+			CheckParametrs(digit, array);
 
 			List<int> result = new List<int>();
-			foreach(var number in list)
+			foreach(var number in array)
 			{
 				if (ContainsDigit(number, digit))
 					result.Add(number);
@@ -21,15 +27,26 @@ namespace Filter
 			return result.ToArray();
 		}
 
-		private static void CheckParametrs(int digit, int[] list)
+		/// <summary>
+		/// Checks if passed digit and array are valid.
+		/// </summary>
+		/// <param name="digit">Digit to filter.</param>
+		/// <param name="array">Array to filter.</param>
+		private static void CheckParametrs(int digit, int[] array)
 		{
 			if (digit < 0 || digit > 9)
 				throw new ArgumentException("Integer in range from 0 to 9 inclusevly expected.", nameof(digit));
 
-			if (list == null)
-				throw new ArgumentNullException(nameof(list), "The value can not be undefined");
+			if (array == null)
+				throw new ArgumentNullException(nameof(array), "The value can not be undefined");
 		}
 
+		/// <summary>
+		/// Returns true if passed number contains specified digit.
+		/// </summary>
+		/// <param name="number">The number to check.</param>
+		/// <param name="digit">The digit for checking.</param>
+		/// <returns>True if number contains specified digit, else false.</returns>
 		private static bool ContainsDigit(int number, int digit)
 		{
 			List<int> digits = new List<int>();
@@ -52,6 +69,11 @@ namespace Filter
 			return false;
 		}
 
+		/// <summary>
+		/// Convert list of integers to array.
+		/// </summary>
+		/// <param name="list">List of integers to convert.</param>
+		/// <returns>Array of integers.</returns>
 		private static int[] ToArray(List<int> list)
 		{
 			var array = new int[list.Count];
