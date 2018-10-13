674c6a2b44bc903b566abae659dd25f9c5acc049
Vika <kaledaviktoriya@gmail.com>
2018-10-13 13:53:56 +0300

    Day 02: Add NthRoot class

---
+++ NET.W.2018.Kaleda.02/NthRoot/NthRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthRoot
+{
+    public static class NthRoot
+    {
+		public static double FindNthRoot(double number, int n, double accuracy)
+		{
+			return 0;
+		}
+    }
+}
