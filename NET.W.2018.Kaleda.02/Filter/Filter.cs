da504be58ca31b08d759466be657e21b4bd396c2
Vika <kaledaviktoriya@gmail.com>
2018-10-13 11:03:31 +0300

    Day 02: Create Filter class

---
+++ NET.W.2018.Kaleda.02/Filter/Filter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter
+{
+    public static class Filter
+    {
+		public static IEnumerable<int> FilterDigit(IEnumerable<int> list)
+		{
+			return new List<int>();
+		}
+    }
+}
