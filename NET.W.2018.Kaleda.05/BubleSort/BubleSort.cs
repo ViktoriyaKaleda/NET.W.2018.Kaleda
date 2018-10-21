2cd29c46622cc96723f6cb43bfe493bbb5839921
Vika <kaledaviktoriya@gmail.com>
2018-10-21 20:27:53 +0300

    Day 05: Add sorting realization and tests for it

---
+++ NET.W.2018.Kaleda.05/BubleSort/BubleSort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubleSort
+{
+	/// <summary>
+	/// Class for sorting jagged arrays.
+	/// </summary>
+	public static class BubleSort
+    {
+		/// <summary>
+		/// Sorts jagged array.
+		/// </summary>
+		/// <param name="array">The array to sort.</param>
+		/// <param name="comparer">Comparer which contains sorting logic.</param>
+		/// <exception cref="ArgumentNullException">Throws when array or comparer are null.</exception>
+		/// <exception cref="ArgumentException">Throws when array is empty.</exception>
+		public static void Sort(int[][] array, IComparer<int[]> comparer)
+		{
+			if (array == null || comparer == null)
+				throw new ArgumentNullException(array == null ? nameof(array) : nameof(comparer), "The value can not be undefined.");
+
+			if (array.Length == 0)
+				throw new ArgumentException("Value can not be empty.", nameof(array));
+
+			SortRealization(array, comparer);
+		}
+
+		/// <summary>
+		/// Bumble sorting realization.
+		/// </summary>
+		/// <param name="array">>The array to sort.</param>
+		/// <param name="comparer">Comparer which contains sorting logic.</param>
+		private static void SortRealization(int[][] array, IComparer<int[]> comparer)
+		{
+			for (int i = 0; i < array.Length - 1; i++)
+			{
+				for (int j = 0; j < array.Length - 1; j++)
+				{
+					if (comparer.Compare(array[j], array[j + 1]) > 0)
+						Swap(ref array[j], ref array[j + 1]);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Swaps two arrays.
+		/// </summary>
+		/// <param name="a">The first array.</param>
+		/// <param name="b">The second array.</param>
+		private static void Swap(ref int[] a, ref int[] b)
+		{
+			var temp = a;
+			a = b;
+			b = temp;
+		}
+    }
+}
---
+++ NET.W.2018.Kaleda.05/Sort.NUnitTests/SortTests.cs
@@ -0,0 +1,149 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Sort.NUnitTests
+{
+	[TestFixture]
+	public class SortTests
+	{
+		private static int[][] testArray =
+		{
+			new int[] { 100 },						// Max = 100, Min = 100,  Sum = 100
+			new int[] { 1, 2, 3 },                  // Max = 3,   Min = 1,    Sum = 6
+            new int[] { -1, 2, 10 },				// Max = 10,  Min = -1,   Sum = 11
+            new int[] { 5, 6, 7, 8, 9 },            // Max = 9,   Min = 5,    Sum = 34
+        };
+
+		private static int[][] testArraySortedByAscendingSum =
+		{
+			new int[] { 1, 2, 3 },
+			new int[] { -1, 2, 10 },
+			new int[] { 5, 6, 7, 8, 9 },
+			new int[] { 100 },
+		};
+
+		private static int[][] testArraySortedByDescendingSum =
+		{
+			new int[] { 100 },
+			new int[] { 5, 6, 7, 8, 9 },			
+			new int[] { -1, 2, 10 },
+			new int[] { 1, 2, 3 },
+		};
+
+		private static int[][] testArraySortedByAscendingMax =
+		{
+			new int[] { 1, 2, 3 },
+			new int[] { 5, 6, 7, 8, 9 },
+			new int[] { -1, 2, 10 },
+			new int[] { 100 },
+		};
+
+		private static int[][] testArraySortedByDescendingMax =
+		{
+			new int[] { 100 },
+			new int[] { -1, 2, 10 },
+			new int[] { 5, 6, 7, 8, 9 },
+			new int[] { 1, 2, 3 },
+		};
+
+		private static int[][] testArraySortedByAscendingMin =
+		{
+			new int[] { -1, 2, 10 },
+			new int[] { 1, 2, 3 },
+			new int[] { 5, 6, 7, 8, 9 },
+			new int[] { 100 },
+		};
+
+		private static int[][] testArraySortedByDescendingMin =
+		{
+			new int[] { 100 },
+			new int[] { 5, 6, 7, 8, 9 },
+			new int[] { 1, 2, 3 },
+			new int[] { -1, 2, 10 },
+		};
+
+		[Test]
+		public void Sort_NullParameters_ArgumentNullException()
+		{
+			Assert.Throws<ArgumentNullException>(() => BubleSort.BubleSort.Sort(null, new Comparers.ByAscendingMaxElements()));
+			Assert.Throws<ArgumentNullException>(() => BubleSort.BubleSort.Sort(testArray, null));
+		}
+
+		[Test]
+		public void Sort_SortByAscendingSum_ValidResult()
+		{
+			var comparer = new Comparers.ByAscendingRowElementsSums();
+			var result = GetCopy(testArray);
+
+			BubleSort.BubleSort.Sort(result, comparer);
+
+			CollectionAssert.AreEqual(testArraySortedByAscendingSum, result);
+		}
+
+		[Test]
+		public void Sort_SortByDescendingSum_ValidResult()
+		{
+			var comparer = new Comparers.ByDescendingRowElementsSums();
+			var result = GetCopy(testArray);
+
+			BubleSort.BubleSort.Sort(result, comparer);
+
+			CollectionAssert.AreEqual(testArraySortedByDescendingSum, result);
+		}
+
+		[Test]
+		public void Sort_SortByAscendingMax_ValidResult()
+		{
+			var comparer = new Comparers.ByAscendingMaxElements();
+			var result = GetCopy(testArray);
+
+			BubleSort.BubleSort.Sort(result, comparer);
+
+			CollectionAssert.AreEqual(testArraySortedByAscendingMax, result);
+		}
+
+		[Test]
+		public void Sort_SortByDescendingMax_ValidResult()
+		{
+			var comparer = new Comparers.ByDescendingMaxElements();
+			var result = GetCopy(testArray);
+
+			BubleSort.BubleSort.Sort(result, comparer);
+
+			CollectionAssert.AreEqual(testArraySortedByDescendingMax, result);
+		}
+
+		[Test]
+		public void Sort_SortByAscendingMin_ValidResult()
+		{
+			var comparer = new Comparers.ByAscendingMinElements();
+			var result = GetCopy(testArray);
+
+			BubleSort.BubleSort.Sort(result, comparer);
+
+			CollectionAssert.AreEqual(testArraySortedByAscendingMin, result);
+		}
+		[Test]
+		public void Sort_SortByDescendingMin_ValidResult()
+		{
+			var comparer = new Comparers.ByDescendingMinElements();
+			var result = GetCopy(testArray);
+
+			BubleSort.BubleSort.Sort(result, comparer);
+
+			CollectionAssert.AreEqual(testArraySortedByDescendingMin, result);
+		}
+
+		private int[][] GetCopy(int[][] array)
+		{
+			int[][] copy = new int[array.Length][];
+			for (int i = 0; i < array.Length; i++)
+			{
+				copy[i] = new int[array[i].Length];
+				for (int j = 0; j < copy[i].Length; j++)
+					copy[i][j] = array[i][j];
+			}
+			return copy;
+		}
+	}
+}
