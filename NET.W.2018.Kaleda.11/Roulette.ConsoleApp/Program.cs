8425c7c6479793de29611c51bd5d429385f84bc5
Unknown <kaledaviktoriya@gmail.com>
2018-11-11 21:18:30 +0300

    Day 11: Add console app for testing roulette game

---
+++ NET.W.2018.Kaleda.11/Roulette.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Roulette.Listeners;
+
+namespace Roulette.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var roulette = new Roulette();
+
+			var listeners = new List<IRouletteSpinListener>
+			{
+				new BlackNumbersListener(),
+				new RedNumbersListener(),
+				new EvenNumbersListener(),
+				new OddNumbersListener(),
+				new NumberListener(0)
+			};
+
+			foreach (var listener in listeners)
+				listener.Subscribe(roulette);
+
+			for (int i = 0; i < 50; i++)
+				roulette.Spin();
+		}
+	}
+}
