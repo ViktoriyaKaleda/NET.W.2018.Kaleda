5bbc9db5314193574478e1ed59158d7701e0744d
Unknown <kaledaviktoriya@gmail.com>
2018-11-15 14:27:58 +0300

    Day 12-13: Add xml docs for BinarySearchHelper class

--- NET.W.2018.Kaleda.12-13/BinarySearch/BinarySearchHelper.cs
+++ NET.W.2018.Kaleda.12-13/BinarySearch/BinarySearchHelper.cs
@@ -3,8 +3,26 @@ using System.Collections.Generic;
 
 namespace BinarySearch
 {
+	/// <summary>
+	/// Class that contains methods for searching elements in array using binary search algorithm.
+	/// </summary>
 	public static class BinarySearchHelper
     {
+		/// <summary>
+		/// Returns index of the given element or -1 if it is not exists. If comparer is null, elements of
+		/// the list are compared to the search value using the IComparable interface. This method assumes 
+		/// that the given section of the list is already sorted; if not, the result will be incorrect.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items">The source array.</param>
+		/// <param name="index">The start index in array for search.</param>
+		/// <param name="count">The number of elements in source array for search.</param>
+		/// <param name="item">The item for search.</param>
+		/// <param name="comparer">The comparer.</param>
+		/// <exception cref="ArgumentOutOfRangeException">Throws when passed index or 
+		/// count are negative.</exception>
+		/// <exception cref="ArgumentException">Throws when invalid interval for search are passed.</exception>
+		/// <returns>Returns index of the given element or -1 if it is not exists.</returns>
 		public static int BinarySearch<T>(this T[] items, int index, int count, T item, IComparer<T> comparer)
 		{
 			if (index < 0 || count < 0)
@@ -21,6 +39,14 @@ namespace BinarySearch
 			return Search(items, index, count, item, comparer);
 		}
 
+		/// <summary>
+		/// Returns index of the given element or -1 if it is not exists. This method uses default comparer. This method assumes 
+		/// that the given section of the list is already sorted; if not, the result will be incorrect.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items">The source array.</param>
+		/// <param name="item">The item for search.</param>
+		/// <returns></returns>
 		public static int BinarySearch<T>(this T[] items, T item)
 		{
 			CheckArrayOnNull<T>(items);
@@ -30,6 +56,15 @@ namespace BinarySearch
 			return Search(items, 0, items.Length, item, comparer);
 		}
 
+		/// <summary>
+		///Returns index of the given element or -1 if it is not exists. This method uses default comparer. This method assumes 
+		/// that the given section of the list is already sorted; if not, the result will be incorrect.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items">The source array.</param>
+		/// <param name="item">The item for search.</param>
+		/// <param name="comparer">The comparer.</param>
+		/// <returns></returns>
 		public static int BinarySearch<T>(this T[] items, T item, IComparer<T> comparer)
 		{
 			CheckArrayOnNull<T>(items);
@@ -40,6 +75,17 @@ namespace BinarySearch
 			return Search(items, 0, items.Length, item, comparer);
 		}
 
+		/// <summary>
+		/// The realization of binary search algorithm. This method assumes 
+		/// that the given section of the list is already sorted; if not, the result will be incorrect.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items">The source array.</param>
+		/// <param name="index">The start index in array for search.</param>
+		/// <param name="count">The number of elements in source array for search.</param>
+		/// <param name="item">The item for search.</param>
+		/// <param name="comparer">The comparer.</param>
+		/// <returns></returns>
 		private static int Search<T>(T[] items, int index, int count, T item, IComparer<T> comparer)
 		{
 			int left = index;
@@ -63,6 +109,10 @@ namespace BinarySearch
 			return ~left;
 		}
 
+		/// <summary>
+		/// Checks if passed array is not null.
+		/// </summary>
+		/// <param name="items">Array to check.</param>
 		private static void CheckArrayOnNull<T>(T[] items)
 		{
 			if (items == null)
