7cf404a59e1ff71e4b3da66f1a4d2504bc2b915b
Unknown <kaledaviktoriya@gmail.com>
2018-11-15 17:34:31 +0300

    Day 12-13: Add Fibonacci realization

---
+++ NET.W.2018.Kaleda.12-13/Fibonacci/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+	/// <summary>
+	/// Class that contains method for generating fibonacci sequence.
+	/// </summary>
+	public static class Fibonacci
+    {
+		/// <summary>
+		/// Generates fibonacci sequence with passed number of elements.
+		/// </summary>
+		/// <param name="count">The number of elements</param>
+		/// <returns>Fibonacci sequence with passed number of elements</returns>
+		public static IEnumerable<ulong> Generate(int count)
+		{
+			if (count < 1)
+				throw new ArgumentException("Positive integer number expected.", nameof(count));
+
+			ulong a = 0, b = 1;
+			while (count-- > 0)
+			{
+				ulong temp = b;
+				checked
+				{
+					b = a + b;
+				}
+				a = temp;
+				yield return a;
+			}
+		}
+	}
+}
