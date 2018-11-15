8c9990d02c7c98b87612ab880fab02c7f44a44b9
Unknown <kaledaviktoriya@gmail.com>
2018-11-15 17:35:06 +0300

    Day 12-13: Add tests for Fibonacci

---
+++ NET.W.2018.Kaleda.12-13/Fibonacci.Tests/FibonacciTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Fibonacci;
+using System.Linq;
+
+namespace Fibonacci.Tests
+{
+	[TestFixture]
+	public class FibonacciTests
+	{
+		[Test]
+		public void Generate_ValidData_ValidResult()
+		{
+			CollectionAssert.AreEqual(Fibonacci.Generate(1), new int[] { 1 });
+			CollectionAssert.AreEqual(Fibonacci.Generate(5), new int[]{ 1, 1, 2, 3, 5 });
+		}
+
+		[Test]
+		public void Generate_NonPositiveCount_ArgumentException()
+			=> Assert.Throws<ArgumentException>(() => Fibonacci.Generate(0).ToArray());
+
+		[Test]
+		public void Generate_ToBigNumberOfElements_()
+			=> Assert.Throws<OverflowException>(() => Fibonacci.Generate(1000000).ToArray());
+	}
+}
