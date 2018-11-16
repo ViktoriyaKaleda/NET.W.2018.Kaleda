f7d5896ba73b38bb79d42423fc165a6e3d9e70b3
Unknown <kaledaviktoriya@gmail.com>
2018-11-16 23:31:20 +0300

    Day 14: Add tests for queue

---
+++ NET.W.2018.Kaleda.14/Queue.Tests/QueueTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Queue;
+
+namespace Queue.Tests
+{
+	[TestFixture]
+	public class QueueTests
+	{
+		public static int[] source = { 4, 1, 2, 6, 7, 9, 3, 10, 0, 8 };
+
+		[Test]
+		public void Count_ConstructorWithSourceItems_ElementsCount()
+		{
+			var queue = new Queue<int>(source);
+			Assert.That(queue.Count, Is.EqualTo(10));
+		}
+
+		[Test]
+		public void Queue_EnqueueDequeuePeekValues_ValidResult()
+		{
+			var queue = new Queue<int>();
+			foreach (int i in source)
+				queue.Enqueue(i);
+
+			Assert.That(queue.Dequeue(), Is.EqualTo(4));
+			Assert.That(queue.Dequeue(), Is.EqualTo(1));
+			Assert.That(queue.Peek(), Is.EqualTo(2));
+			Assert.That(queue.Peek(), Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Queue_IteratoinOnElements_ValidResult()
+		{
+			var queue = new Queue<int>(source);
+
+			int i = 0;
+			foreach(int item in queue)
+			{
+				Assert.That(item == source[i]);
+				i++;
+			}
+		}
+	}
+}
