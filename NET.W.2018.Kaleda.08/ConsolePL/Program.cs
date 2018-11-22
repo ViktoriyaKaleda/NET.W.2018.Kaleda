f7fc8bca82a7f482eda438848acfe262e62e69bd
Unknown <kaledaviktoriya@gmail.com>
2018-11-22 21:36:27 +0300

    Day 15-16: Change ConsolePL implementation

--- NET.W.2018.Kaleda.08/ConsolePL/Program.cs
+++ NET.W.2018.Kaleda.08/ConsolePL/Program.cs
@@ -1,66 +1,109 @@
 ﻿using BLL.Interface.Entities;
+using BLL.Interface.Interfaces;
 using BLL.Interface.Services;
+using DAL.Interface.Interfaces;
 using DependencyResolver;
 using Ninject;
 using System;
-using System.Collections.Generic;
+using Ninject.Parameters;
+using DAL.Interface.Exceptions;
 
 namespace ConsolePL
 {
 	class Program
 	{
 		private static readonly IKernel _resolver;
-		private static IBankAccountService _bankAccountService;
 
 		static Program()
 		{
 			_resolver = new StandardKernel();
-			_resolver.ConfigurateResolver();
-			_bankAccountService = _resolver.Get<IBankAccountService>();
+			_resolver.ConfigurateResolver();			
 		}
 
 		static void Main(string[] args)
 		{
-			var owner1 = new AccountOwner()
-			{
-				FirstName = "Viktoriya",
-				LastName = "Kaleda",
-			};
+			IBankAccountService _bankAccountService;
 
-			var owner2 = new AccountOwner()
-			{
-				FirstName = "Ivan",
-				LastName = "Ivanov",
-			};
+			_bankAccountService = _resolver.Get<IBankAccountService>(
+				new ConstructorArgument("repository", _resolver.Get<IBankAccountRepository>()),
+				new ConstructorArgument("numberGenerator", _resolver.Get<INumberGenerator>()));
 
-			var accountsNumbers = new List<string>()
+			while (true)
 			{
-				_bankAccountService.CreateNewAccount(BankAccountType.BaseBankAccount, owner1),
-				_bankAccountService.CreateNewAccount(BankAccountType.GoldBankAccount, owner1, balance: 200),
-				_bankAccountService.CreateNewAccount(BankAccountType.PlatinumBankAccount, owner2, balance: 100),
-			};
+				Console.WriteLine("1. Add base account");
+				Console.WriteLine("2. Add gold account");
+				Console.WriteLine("3. Add platinum account");
+				Console.WriteLine("4. List of accounts");
+				Console.WriteLine("5. Wirthdraw money");
+				Console.WriteLine("6. Deposit money");
+				Console.WriteLine("7. Exit");
 
-			PrintAccountsInfo();
+				ConsoleKeyInfo choose = Console.ReadKey();
+				Console.Clear();
 
-			foreach (var number in accountsNumbers)
-			{
-				_bankAccountService.Deposit(number, 150);
+				if (choose.KeyChar == '1')
+				{
+					Console.WriteLine(String.Format("Base account with number {0} was created.",
+						_bankAccountService.CreateNewAccount(BankAccountType.BaseBankAccount, new AccountOwner()
+						{
+							FirstName = Console.ReadLine(),
+							LastName = Console.ReadLine()
+						}))
+					);
+				}
+
+				else if (choose.KeyChar == '2')
+				{
+					Console.WriteLine(String.Format("Gold account with number {0} was created.",
+						_bankAccountService.CreateNewAccount(BankAccountType.GoldBankAccount, new AccountOwner()
+						{
+							FirstName = Console.ReadLine(),
+							LastName = Console.ReadLine()
+						}))
+					);
+				}
+
+				else if (choose.KeyChar == '3')
+				{
+					Console.WriteLine(String.Format("Platinum account with number {0} was created.",
+						_bankAccountService.CreateNewAccount(BankAccountType.PlatinumBankAccount, new AccountOwner()
+						{
+							FirstName = Console.ReadLine(),
+							LastName = Console.ReadLine()
+						}))
+					);
+				}
+
+				else if (choose.KeyChar == '4')
+				{
+					PrintAccountsInfo(_bankAccountService);
+				}
+
+				else if (choose.KeyChar == '5')
+				{
+					Console.WriteLine();
+					_bankAccountService.Withdraw(
+						Console.ReadLine(),
+						decimal.Parse(Console.ReadLine()));
+				}
+
+				else if (choose.KeyChar == '6')
+				{
+					Console.WriteLine();
+					_bankAccountService.Deposit(
+						Console.ReadLine(),
+						decimal.Parse(Console.ReadLine()));
+				}
+
+				else if (choose.KeyChar == '7')
+					break;
 			}
-
-			foreach (var number in accountsNumbers)
-			{
-				_bankAccountService.Withdraw(number, 50);
-			}
-
-			PrintAccountsInfo();
-
-			_bankAccountService.CloseAccount(accountsNumbers[0]);
-
-			PrintAccountsInfo();
 		}
 
-		public static void PrintAccountsInfo()
+		public static void PrintAccountsInfo(IBankAccountService _bankAccountService)
 		{
+			Console.WriteLine();
+			Console.WriteLine("Bank accounts: ");
 			foreach (var account in _bankAccountService.Accounts)
 			{
 				Console.WriteLine(account);
